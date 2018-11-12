5b4cf660cf37e4815e5df0a03da5064174f6379e
raghu5910 <raghuram5910@gmail.com>
2018-11-13 01:03:07 +0530

    STATICFILES_FINDERS

--- vatika_website/settings.py
+++ vatika_website/settings.py
@@ -124,12 +124,16 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_FINDERS = [
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    ]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/raghuparedla/vatika_website/static'
+# STATIC_ROOT = '/home/raghuparedla/vatika_website/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
