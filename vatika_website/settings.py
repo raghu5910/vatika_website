77ef17640d8c151fda3f55656d1db3a1f37b23f3
raghu5910 <raghuram5910@gmail.com>
2018-11-15 00:04:24 +0530

    changes for static root

--- vatika_website/settings.py
+++ vatika_website/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static' # During development when you applied collectstatic
+STATIC_ROOT = '/static/' # During development when you applied collectstatic
 # STATIC_ROOT = '/home/raghuparedla/vatika_website/static' # During Production
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
